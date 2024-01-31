1c1db40f49925979c7b43c65a3338a6132a03d90
dpieski <dpieski@gmail.com>
2024-01-31 11:49:54 -0600

    working with progress bars

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ import argparse
 import sys
 import json
 from datetime import datetime
+import html
+from tqdm import tqdm
+
 
 URLBASE = 'https://files02.tokybook.com/audio/'  # file source directory
+
+class AudioBook:
+    def __init__(self, title, tags, tracklist, location):
+        self.title = title
+        self.tags = tags
+        self.tracklist = tracklist
+        self.location = location
 
 
 # Parse the arguments passed via the command line.
@@ -48,15 +58,63 @@ def main():
         return
     parse_url(inputs.book_url, inputs.output)
 
+        
+def get_audiobook():
+    pass
+
+def get_abook_properties(soup, outpath):
+        # Get the book properties from the ld+json section of the page since it is structured data.
+    book_props = json.loads(
+        "".join(soup.find("script", {"type": "application/ld+json"}).contents))
+
+    # Get the book title from the book props. This should be a consistent way to get the book title.
+    book_title = get_booktitle(book_props)
+    print("Book Title: %s" % book_title)
+
+
+    # Pull the tags for the book. These could be good for searching later.
+    print("Tags: %s" % soup.find_all("span", {"class": "tags-links"})[0].get_text().strip()[5:])
+    tags = soup.find_all(
+        "span", {"class": "tags-links"})[0].get_text().strip()[5:].split(", ")
+    
+    # Get the outputFolder - makes the folder, if the default exists, it will increment
+    # a number at the end so nothing is written over.
+    outputFolder = get_outputfolder(outpath, book_title)
+    
+    
+    res = soup.find_all('script')
+    # There are a lot of 'script' tags in the page. Loop through them and find the one
+    # with the "tracks" list.
+    
+    residx = 0
+    for idx, script in enumerate(res):
+        if "tracks = [" in str(script):
+            # print("Index is: {}.".format(idx))
+            residx = idx
+
+    trackscript = res[residx]  # get the 'script' tag with the tracks list
+    
+    # get the string index starting the list
+    trackidx = trackscript.contents[0].find('tracks = [')+9
+    end = trackscript.contents[0].find(
+        ']', trackidx)+1  # find the end of the list
+    
+    jsonstring = trackscript.contents[0][trackidx:end].replace(
+        '\\n', '')  # get that string of the list
+
+    # convert the string of the list to a real list.
+    jsonstring = jsonstring.replace('\\','')
+    tracklist = ast.literal_eval(jsonstring)
+    return AudioBook(title=book_title, tags=tags, tracklist=tracklist,location=outputFolder)
+
+
 # Parse a particular URL and send files to the outpath.
-
-
 def parse_url(bookURL, outpath):
 
     # Make sure that the domain passed in is one that we can read. If not, end things.
     domain = urlparse(bookURL).netloc
     if not domain == 'tokybook.com':
-        print("Please entere a tokybook URL!")
+        print("Please enter a tokybook URL!")
         return
 
     # Get the URL page and then parse with BS4
@@ -69,46 +127,61 @@ def parse_url(bookURL, outpath):
 
     # Get the book title from the book props. This should be a consistent way to get the book title.
     book_title = get_booktitle(book_props)
+    print("Book Title: %s" % book_title)
 
+
+    # Pull the tags for the book. These could be good for searching later.
+    print("Tags: %s" % soup.find_all("span", {"class": "tags-links"})[0].get_text().strip()[5:])
+    tags = soup.find_all(
+        "span", {"class": "tags-links"})[0].get_text().strip()[5:].split(", ")
+    
     # Get the outputFolder - makes the folder, if the default exists, it will increment
     # a number at the end so nothing is written over.
     outputFolder = get_outputfolder(outpath, book_title)
-
-    # Pull the tags for the book. These could be good for searching later.
-    tags = soup.find_all(
-        "span", {"class": "tags-links"})[0].get_text()[5:].split(", ")
-
+    
+    
     res = soup.find_all('script')
-
     # There are a lot of 'script' tags in the page. Loop through them and find the one
     # with the "tracks" list.
+    
     residx = 0
     for idx, script in enumerate(res):
         if "tracks = [" in str(script):
-            print("Index is: {}.".format(idx))
+            # print("Index is: {}.".format(idx))
             residx = idx
 
     trackscript = res[residx]  # get the 'script' tag with the tracks list
+    
     # get the string index starting the list
     trackidx = trackscript.contents[0].find('tracks = [')+9
     end = trackscript.contents[0].find(
         ']', trackidx)+1  # find the end of the list
+    
     jsonstring = trackscript.contents[0][trackidx:end].replace(
         '\\n', '')  # get that string of the list
 
     # convert the string of the list to a real list.
+    jsonstring = jsonstring.replace('\\','')
     tracklist = ast.literal_eval(jsonstring)
+
 
     # loop through the tracks
     track_props = []
-    for track in tracklist:
+    
+    progbar = tqdm(tracklist, position=0)
+    for track in progbar:
+    # for track in tracklist:
         if not track['name'] == 'welcome':  # Skip the welcome track
             track_title = track['chapter_link_dropbox']
+            progbar.set_description("Downloading: {}".format(book_title))
             # Clean the URL, maybe use URLENCODE later
+            # pg = urljoin(URLBASE, track_title)
             pg = urljoin(URLBASE, track_title.replace('\\', ''))
             track_props.append(
                 {"track_number": track['track'], "track_name": track['name'], "track_duration": track['duration']})
-            download_file(pg, outputFolder)
+            download_file(pg, outputFolder, track['name'])
+    progbar.close()
+    print("\nDownload Complete!\n")
 
     # create and save the properties file to record the properties of the downloaded audio book.
     save_properties(outputFolder, book_props, tags, track_props)
@@ -123,12 +196,14 @@ def save_properties(outputFolder, book_props, tags, track_props):
         json.dump(book_props, f, ensure_ascii=False, indent=4)
 
 
-def get_outputfolder(outpath, dirTitle, x=0):
+def get_outputfolder(outpath, dirTitle="audio-", x=0):
+    # This should be updated to by a better parse-able folder. Compatible with other services....
     folderpath = os.path.join(
         outpath, (dirTitle + (' ' + str(x) if x != 0 else '')).strip())
     if not os.path.exists(folderpath):
         # If it doesn't exist, make the directory
         os.mkdir(folderpath)
+        print("Output Path: {}".format(folderpath))
         return folderpath
     else:
         # If it does exist, try again but increase the number at the end.
@@ -137,18 +212,27 @@ def get_outputfolder(outpath, dirTitle, x=0):
 
 def get_booktitle(book_props):
     # parse through the book properties to get the book title.
+    # print("Parsing....")
     for props in book_props['@graph']:
-        if props['@type'] == "BlogPosting":
-            book_title = props['name']
-            return book_title
+        if props['@type'] == "BreadcrumbList":
+            crumblist = []
+            for prop in props['itemListElement']:
+                crumblist.append(prop['item']['name'])
+            
+            # There are two names in crumblist, [0] = 'Home', [1] = Title - they are 'HTML Safe'
+            book_title = html.unescape(crumblist[1])
+            # print(book_title)
+    return book_title
 
 
-def download_file(url, outdir):
+def download_file(url, outdir, name):
     local_filename = url.split('/')[-1]
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True) as r:
+        file_bytes = int(r.headers.get("content-length",0))
         r.raise_for_status()
-        with open(os.path.join(outdir, local_filename), 'wb') as f:
+        with tqdm.wrapattr(open(os.path.join(outdir, local_filename), 'wb'), 'write',
+                           miniters=1, desc="Downloading "+ name, total=file_bytes, position=1) as f:
             for chunk in r.iter_content(chunk_size=8192):
                 # If you have chunk encoded response uncomment if
                 # and set chunk_size parameter to None.
